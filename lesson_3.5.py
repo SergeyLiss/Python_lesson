73c2bf7cf2aea6ceb4ca2219cbe925f53ce20e7b
SergeyR <slog18122018@gmail.com>
2022-12-21 12:40:52 +0300

    added реализация 2 для lesson_3.5.py

--- lesson_3.5.py
+++ lesson_3.5.py
@@ -1,8 +1,8 @@
 # Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 
-number = 8
+number = -8
 
-def NegaFibonacci(num):
+def NegaFibonacci(num): # Реализация 1
     array = [0 for i in range((num<<1)+1)]
     array[num+1] = 1
     array[num-1] = 1
@@ -13,4 +13,14 @@ def NegaFibonacci(num):
 
     return array
 
+def Fibonachi(number): # Реализация 2
+    f = 1.6180339887498948482045868343656 # Золотое сечение
+    fresult = int((pow(f,number) - pow(-f, -number)) / (2 * f - 1)) # Формула Бине
+    return fresult
+
+
+farray = [Fibonachi(i) for i in range(-number, number)]
+
+
 print(NegaFibonacci(number))
+print(farray)
